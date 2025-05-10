c2c42410cf84965c1aaac1ae336a878c4f5eeb50
Connor Dilgren <cdilgren@umd.edu>
2025-05-10 18:35:30 +0000

    run evaluate_compute with synchronize steps

--- methods/pca_topk/attention_benchmark.py
+++ methods/pca_topk/attention_benchmark.py
@@ -128,62 +128,62 @@ def micro_benchmark_pca_topk(cache, prompt_keys, top_r, top_k, num_layers, timer
                 generative_query, generative_key, generative_value = dense_projs[layer](input_embedding)
                 timers.stop('qk-gen')
 
-                # timers.start('project')
-                # generative_key = generative_key.squeeze().transpose(0, 1).bmm(pca_projection_mat).unsqueeze(2)
-                # generative_query = generative_query.squeeze().transpose(0, 1).bmm(pca_projection_mat).unsqueeze(2)
-                # generative_value = generative_value.squeeze().transpose(0, 1).bmm(pca_projection_mat).unsqueeze(2)
-                # timers.stop('project')
+                timers.start('project')
+                generative_key = generative_key.squeeze().transpose(0, 1).bmm(pca_projection_mat).unsqueeze(2)
+                generative_query = generative_query.squeeze().transpose(0, 1).bmm(pca_projection_mat).unsqueeze(2)
+                generative_value = generative_value.squeeze().transpose(0, 1).bmm(pca_projection_mat).unsqueeze(2)
+                timers.stop('project')
 
-                # timers.start('cache-update')
-                # keys, vals = cache.update(generative_key, generative_value, generative_query, layer, False)
-                # timers.stop('cache-update')
+                timers.start('cache-update')
+                keys, vals = cache.update(generative_key, generative_value, generative_query, layer, False)
+                timers.stop('cache-update')
 
-                # timers.start('qk-matmul-1')
-                # nh, bs, s, r = keys.shape
-                # attn_weights = G.topr_bmv_optimized(A=generative_query.view(nh*bs, 1, r), B=keys.view(nh*bs, s, r).transpose(-1,-2), 
-                #                                     r=top_r)
-                # attn_weights = attn_weights.view(nh, bs, 1, s)
-                # timers.stop('qk-matmul-1')
+                timers.start('qk-matmul-1')
+                nh, bs, s, r = keys.shape
+                attn_weights = G.topr_bmv_optimized(A=generative_query.view(nh*bs, 1, r), B=keys.view(nh*bs, s, r).transpose(-1,-2), 
+                                                    r=top_r)
+                attn_weights = attn_weights.view(nh, bs, 1, s)
+                timers.stop('qk-matmul-1')
 
-                # # Get top-k keys and top-k values based on the attention scores
-                # timers.start('top-k')
-                # key_states_topk_indices = torch.argsort(attn_weights, dim=-1, descending=True)[:,:,:,:top_k]
-                # timers.stop('top-k')
+                # Get top-k keys and top-k values based on the attention scores
+                timers.start('top-k')
+                key_states_topk_indices = torch.argsort(attn_weights, dim=-1, descending=True)[:,:,:,:top_k]
+                timers.stop('top-k')
 
-                # timers.start('reshape-0')
-                # key_states_topk_indices= key_states_topk_indices.reshape(-1, key_states_topk_indices.shape[-1])
-                # timers.stop('reshape-0')
+                timers.start('reshape-0')
+                key_states_topk_indices= key_states_topk_indices.reshape(-1, key_states_topk_indices.shape[-1])
+                timers.stop('reshape-0')
 
-                # timers.start('reshape-1')
-                # keys = keys.view(-1, keys.shape[-2] , keys.shape[-1])
-                # vals = vals.view(-1, vals.shape[-2] , vals.shape[-1])
-                # timers.stop('reshape-1')
+                timers.start('reshape-1')
+                keys = keys.view(-1, keys.shape[-2] , keys.shape[-1])
+                vals = vals.view(-1, vals.shape[-2] , vals.shape[-1])
+                timers.stop('reshape-1')
 
-                # timers.start('qk-matmul-2')
-                # attn_weights = G.gather_outer_bmv_optimized(
-                #     generative_query.reshape(-1, 1, head_dim),
-                #     keys.transpose(-1, -2),
-                #     key_states_topk_indices,
-                #     #.squeeze(0).squeeze(-1),
-                #     #chunk=256
-                #     #chunk=min(k2, 65536 // Q.shape[-1]),
-                # ) / math.sqrt(head_dim)
-                # timers.stop('qk-matmul-2')
+                timers.start('qk-matmul-2')
+                attn_weights = G.gather_outer_bmv_optimized(
+                    generative_query.reshape(-1, 1, head_dim),
+                    keys.transpose(-1, -2),
+                    key_states_topk_indices,
+                    #.squeeze(0).squeeze(-1),
+                    #chunk=256
+                    #chunk=min(k2, 65536 // Q.shape[-1]),
+                ) / math.sqrt(head_dim)
+                timers.stop('qk-matmul-2')
 
-                # timers.start('softmax')
-                # attn_weights = torch.softmax(attn_weights.float(), dim=-1).to(dtype)
-                # timers.stop('softmax')
+                timers.start('softmax')
+                attn_weights = torch.softmax(attn_weights.float(), dim=-1).to(dtype)
+                timers.stop('softmax')
 
-                # timers.start('sv-matmul')
-                # attn_output = G.gather_inner_matrix_only_bmv_optimized(
-                #     attn_weights, vals, key_states_topk_indices)
-                # timers.stop('sv-matmul')
+                timers.start('sv-matmul')
+                attn_output = G.gather_inner_matrix_only_bmv_optimized(
+                    attn_weights, vals, key_states_topk_indices)
+                timers.stop('sv-matmul')
 
-                # timers.start('reshape-output')
-                # attn_output = attn_output.view(num_heads, bs, 1, head_dim).transpose(0,1).transpose(1,2).contiguous()
-                # timers.stop('reshape-output')
+                timers.start('reshape-output')
+                attn_output = attn_output.view(num_heads, bs, 1, head_dim).transpose(0,1).transpose(1,2).contiguous()
+                timers.stop('reshape-output')
 
-                # input_embedding = attn_output.transpose(1, 2).contiguous()  # reset the shape to the original shape
+                input_embedding = attn_output.transpose(1, 2).contiguous()  # reset the shape to the original shape
 
         timers.stop('total')
     else:
@@ -368,7 +368,7 @@ def micro_bench_actual_attention(cache, prompt_keys, num_layers, timers, num_gen
           attn_output = attn_output.transpose(1, 2).contiguous()
           timers.stop('reshape-output')
 
-        #   input_embedding = attn_output.transpose(1, 2).contiguous()  # reset the shape to the original shape
+          input_embedding = attn_output.transpose(1, 2).contiguous()  # reset the shape to the original shape
 
     timers.stop('total')
 
