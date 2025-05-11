ce91f6ab55d3ac03fe5fcc17cc86c98f203bb285
Connor Dilgren <cdilgren@umd.edu>
2025-05-11 17:02:54 +0000

    test synchronize accuracy in qk-gen

--- evaluate_compute.py
+++ evaluate_compute.py
@@ -10,26 +10,26 @@ torch.backends.cudnn.allow_tf32 = False
 
 if __name__ == "__main__":
     with torch.no_grad():
-        # for prompt_length in [512, 1024]:
-        for prompt_length in [2048, 3072]:
-            for num_gen_steps in [128, 256, 512]:
-            # for num_gen_steps in [128]:
+        for prompt_length in [512]:
+        # for prompt_length in [2048, 3072]:
+            # for num_gen_steps in [128, 256, 512]:
+            for num_gen_steps in [128]:
                 # does not depend on topk, topr, stride
                 _, times_vanilla, _ = benchmark_attention(prompt_length=prompt_length, num_gen_steps=num_gen_steps, batch_size=16, pcatopk=False, sparse_transformer=False)
-                with open(f"compute_files/vanilla_prompt_{prompt_length}_gen_{num_gen_steps}.json", "w") as f:
-                    json.dump(times_vanilla, f, indent=2)
+                # with open(f"compute_files/vanilla_prompt_{prompt_length}_gen_{num_gen_steps}.json", "w") as f:
+                #     json.dump(times_vanilla, f, indent=2)
 
-                for topk in [4, 8]:
-                # for topk in [8]:
-                    for topr in [4]:
-                        # does not depend on stride
-                        print(f"prompt length = {prompt_length}, gen length = {num_gen_steps}, batch_size={16}, topk={topk} and topr={topr}")
-                        times_pca_topk, _, _ = benchmark_attention(prompt_length=prompt_length, num_gen_steps=num_gen_steps, batch_size=16, topk=prompt_length // topk, topr=128 // topr, vanilla=False, sparse_transformer=False)
-                        with open(f"compute_files/loki_prompt_{prompt_length}_gen_{num_gen_steps}_topk_{topk}_topr_{topr}.json", "w") as f:
-                            json.dump(times_pca_topk, f, indent=2)
+                # for topk in [4, 8]:
+                # # for topk in [8]:
+                #     for topr in [4]:
+                #         # does not depend on stride
+                #         print(f"prompt length = {prompt_length}, gen length = {num_gen_steps}, batch_size={16}, topk={topk} and topr={topr}")
+                #         times_pca_topk, _, _ = benchmark_attention(prompt_length=prompt_length, num_gen_steps=num_gen_steps, batch_size=16, topk=prompt_length // topk, topr=128 // topr, vanilla=False, sparse_transformer=False)
+                #         with open(f"compute_files/loki_prompt_{prompt_length}_gen_{num_gen_steps}_topk_{topk}_topr_{topr}.json", "w") as f:
+                #             json.dump(times_pca_topk, f, indent=2)
 
-                        for stride in [512]:
-                            print(f"prompt length = {prompt_length}, gen length = {num_gen_steps}, batch_size={16}, topk={topk} and topr={topr}, stride={stride}")
-                            _, _, times_sparse_pca_topk = benchmark_attention(prompt_length=prompt_length, num_gen_steps=num_gen_steps, batch_size=16, topk=prompt_length // topk, topr=128 // topr, stride=stride, pcatopk=False, vanilla=False)
-                            with open(f"compute_files/sparse_transformer_loki_prompt_{prompt_length}_gen_{num_gen_steps}_topk_{topk}_topr_{topr}_stride_{stride}.json", "w") as f:
-                                json.dump(times_sparse_pca_topk, f, indent=2)
+                #         for stride in [512]:
+                #             print(f"prompt length = {prompt_length}, gen length = {num_gen_steps}, batch_size={16}, topk={topk} and topr={topr}, stride={stride}")
+                #             _, _, times_sparse_pca_topk = benchmark_attention(prompt_length=prompt_length, num_gen_steps=num_gen_steps, batch_size=16, topk=prompt_length // topk, topr=128 // topr, stride=stride, pcatopk=False, vanilla=False)
+                #             with open(f"compute_files/sparse_transformer_loki_prompt_{prompt_length}_gen_{num_gen_steps}_topk_{topk}_topr_{topr}_stride_{stride}.json", "w") as f:
+                #                 json.dump(times_sparse_pca_topk, f, indent=2)
--- methods/common/timers.py
+++ methods/common/timers.py
@@ -6,8 +6,9 @@ class Timers():
         self.timers = defaultdict(list)
         self.curr_index = defaultdict(int)
 
-    def start(self, key):
-        torch.cuda.synchronize()
+    def start(self, key, sync=True):
+        if sync:
+            torch.cuda.synchronize()
         index = self.curr_index[key]
         timers = self.timers[key]
         assert index == len(timers) or index < len(timers)
@@ -16,8 +17,9 @@ class Timers():
         self.timers[key][index][0].record()
 
 
-    def stop(self, key):
-        torch.cuda.synchronize()
+    def stop(self, key, sync=True):
+        if sync:
+            torch.cuda.synchronize()
         index = self.curr_index[key]
         self.timers[key][index][1].record()
         self.curr_index[key] += 1
